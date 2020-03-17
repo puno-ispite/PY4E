c1e19d9d2465fe2d66d2e8b4383862a807654206
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-16 18:03:16 -0700

    Create Functions1.py

---
+++ Course 1 - Getting Started with Python/Chapter 4/Lecture Material/Functions1.py
@@ -0,0 +1,20 @@
+'''Functions'''
+
+def thing() :
+    print("Hello")
+    print("Fun!")
+  
+thing()
+print('Zip')
+thing()
+
+'''Output:
+Hello
+Fun!
+Zip
+Hello
+Fun '''
+
+big = max('Hello world') #built in function
+print(big)
+#w
