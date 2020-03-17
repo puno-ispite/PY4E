88aeeb76973353a30c9e35ea1bd4efc5ff2d1b75
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-16 17:10:47 -0700

    Create ConditionalStatements1.py

---
+++ Course 1 - Getting Started with Python/Chapter 3/Lecture Material/ConditionalStatements1.py
@@ -0,0 +1,9 @@
+'''Basic Conditional Execution'''
+x = 5
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+if x != 5:
+ print(x) #a single space is enough to indent a line into a condition
+print('Finish!')
