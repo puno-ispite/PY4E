af61a4265037b91e0839b7218858b7b01df68b20
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-16 18:45:53 -0700

    Create DefiniteLoops.py

---
+++ Course 1 - Getting Started with Python/Chapter 5/Lecture Material/DefiniteLoops.py
@@ -0,0 +1,13 @@
+'''A Simple Definite For Loop'''
+
+for i in [5,4,3,2,1] :
+    print(i)
+print('Blastoff!\n')
+##countdown + blastoff string
+
+'''A Definite Loop with Strings'''
+
+friends = ['Glenn', 'Jaewon', 'Joe Biden']
+for friend in friends :
+    print('Stay safe from COVID-19:', friend)
+print('Done!')
