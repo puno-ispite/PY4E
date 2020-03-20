b30841f8d0914db7257717c84d03e481d0a44ead
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-20 12:15:23 -0700

    Create FollowingLinks-HTML.py

---
+++ Course 3 - Using Python to Access Web Data/Chapter 12/Assignments/FollowingLinks-HTML.py
@@ -0,0 +1,78 @@
+'''Following Links in Python
+
+In this assignment you will write a Python program that expands on http://www.py4e.com/code3/urllinks.py. The program will use urllib to read the HTML from the data files below, extract the href= vaues from the anchor tags, scan for a tag that is in a particular position relative to the first name in the list, follow that link and repeat the process a number of times and report the last name you find.
+
+We provide two files for this assignment. One is a sample file where we give you the name for your testing and the other is the actual data you need to process for the assignment
+
+Sample problem: Start at http://py4e-data.dr-chuck.net/known_by_Fikret.html
+Find the link at position 3 (the first name is 1). Follow that link. Repeat this process 4 times. The answer is the last name that you retrieve.
+Sequence of names: Fikret Montgomery Mhairade Butchi Anayah
+Last name in sequence: Anayah
+Actual problem: Start at: http://py4e-data.dr-chuck.net/known_by_Freia.html
+Find the link at position 18 (the first name is 1). Follow that link. Repeat this process 7 times. The answer is the last name that you retrieve.
+Hint: The first character of the name of the last page that you will load is: D
+Strategy
+The web pages tweak the height between the links and hide the page after a few seconds to make it difficult for you to do the assignment without writing a Python program. But frankly with a little effort and patience you can overcome these attempts to make it a little harder to complete the assignment without writing a Python program. But that is not the point. The point is to write a clever Python program to solve the program.
+
+Sample execution
+
+Here is a sample execution of a solution:
+
+$ python3 solution.py
+Enter URL: http://py4e-data.dr-chuck.net/known_by_Fikret.html
+Enter count: 4
+Enter position: 3
+Retrieving: http://py4e-data.dr-chuck.net/known_by_Fikret.html
+Retrieving: http://py4e-data.dr-chuck.net/known_by_Montgomery.html
+Retrieving: http://py4e-data.dr-chuck.net/known_by_Mhairade.html
+Retrieving: http://py4e-data.dr-chuck.net/known_by_Butchi.html
+Retrieving: http://py4e-data.dr-chuck.net/known_by_Anayah.html
+The answer to the assignment for this execution is "Anayah".'''
+
+# To run this, download the BeautifulSoup zip file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl, re
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL: ')
+count = int(input('Enter Count: '))
+position = int(input('Enter Position: '))
+
+index = 0
+names = list()
+
+while index < count :
+    if(index != 0):
+        url = names[index-1][1]
+        #print("Retrieving:", url)
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Retrieve all of the anchor tags
+    tags = soup('a')
+    iterVar = 1
+    for tag in tags:
+        if(iterVar == position) :
+            names.append([tag.contents[0], tag.get('href', None)])
+            #print(names[index-1][0])
+            #names[tag.contents[0]] = names.get( tag.contents[0], tag.get('href', None))
+            break
+        iterVar += 1
+    index += 1
+
+for i in names :
+    print(i)
+
+print('\nLast Name Retrieved:', names[count-1][0], "\tURL:", names[count-1][1])
+
+#http://py4e-data.dr-chuck.net/known_by_Freia.html
+#Repeat 7 times
+#Position 18
