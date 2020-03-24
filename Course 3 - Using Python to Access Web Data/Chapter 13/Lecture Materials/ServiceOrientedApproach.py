2c179c1e57664f65c560989240f50434fa8ee759
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-23 22:14:15 -0700

    Create ServiceOrientedApproach.py

---
+++ Course 3 - Using Python to Access Web Data/Chapter 13/Lecture Materials/ServiceOrientedApproach.py
@@ -0,0 +1,10 @@
+'Service Oriented Approach'
+#Most non-trivial web applications use services
+#They use services form other applications
+#   -Credit Card Charge
+#   -Hotel Reservation systems
+#Services publish the "rules" applications must follow to make use of the service (API)
+
+'Multiple Systems'
+#Initially - two systems cooperate and split the problem.
+#As the data/service becomes useful - multiple applications want to use the information application
