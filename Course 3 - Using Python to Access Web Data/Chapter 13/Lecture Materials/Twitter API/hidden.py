3c3406c50ea63323029f5e416133c77c2e38bc3f
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-24 22:09:36 -0700

    Create hidden.py

---
+++ Course 3 - Using Python to Access Web Data/Chapter 13/Lecture Materials/Twitter API/hidden.py
@@ -0,0 +1,10 @@
+#Keep this file separate
+
+#https://apps.twitter.com/
+#Create new App and get the four strings
+
+def oauth() : #SENSITIVE INFORMATION CENSORED
+  return {"consumer_key": "owk7....ou6",
+          "consumer_secret": "lk6vN8YRz.....RaENdaIfaS",
+          "token_key": "116586423.......0lU7ADle7pw4",
+          "token_secret" : "w12Ynt.zysd" }
