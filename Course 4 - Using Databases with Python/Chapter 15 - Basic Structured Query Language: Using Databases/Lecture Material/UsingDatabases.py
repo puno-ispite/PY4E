dc594037e94d8cc33c03b027ac9892f0c1deb792
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-27 15:53:58 -0700

    Create UsingDatabases.py

---
+++ Course 4 - Using Databases with Python/Chapter 15 - Basic Structured Query Language: Using Databases/Lecture Material/UsingDatabases.py
@@ -0,0 +1,22 @@
+'Two Roles in Large Projects'
+#Application Developer - Builds the logic for the application, the look and feel of the application - monitors the application for problems
+#*Database Administrator - Monitors and adjusts the databases as the program runs in production
+#Often both participate in the building of the "Data model"
+
+'Database Administrator (dba)'
+#A database administrator (DBA) is a person responsible for the design, implementation, maintenance, and repair
+#of an organization's database. The role includes the development and deisgn of database strategies, monitoring and improving database performance
+#and capacity, and planning for future expansion requirements. They may also plan, coordinate, and implement security measures to safeguard the database.
+
+'Database Model'
+#A database model or database schema is the structure or format of a database, described in a formal language supported by the database management
+#sytem. In other words, a "database model" is the application of a data model when used in conjunction with a database management system.
+
+'Common Database Systems'
+#Three Major Database Management Systems in wide use
+#   -Oracle - Large, commercial, enterprise-scale, very very tweakable.
+#   -MySql - Simpler but very fast and scalable - commercial open source.
+#   -SqlServer - Very nice - from Microsoft (also Access)
+# Many other smaller projects, free and open source
+#   -HSQL, SQLite, Postgress, ...
+#SQLite is what's called an embedded database. Very small.
