8031ecc3be7616b96716e7cc41d5cb8caee18fa7
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-27 15:12:09 -0700

    Create RelationalDatabases.py

---
+++ Course 4 - Using Databases with Python/Chapter 15 - Basic Structured Query Language: Using Databases/Lecture Material/RelationalDatabases.py
@@ -0,0 +1,31 @@
+'SQLite Browser'
+#http://sqlitebrowser.org/
+#Or you can get the Google Chrome Plugin
+
+'Relational Databases'
+#Relational databases model data by storing rows and columns in tables. The power of the relational database lies in its ability
+#to efficiently retrieve data from those tables and in particular where there are multiple tables and the relationships
+#between those tables involved in the query.
+
+'Terminology'
+#Database - contains many tables
+#Relation (or table) - contains tuples and attributes
+#Tuple (or row) - a set of fields that generally represents an "object" like a person or a music track.
+#Attribute (also column or field) - one of possible many elements of data corresponding to the object represented by tje row
+'''   attribute
+_ _ _ _ _ # _ _ 
+_ _ _ _ _ # _ _
+# # # # # # # # Tuple
+_ _ _ _ _ # _ _
+^---Relation--^
+'''
+#A relation is defined as a set of tuples that have the same attributes. A tuple usually represents an object and information
+#about that object. Objects are typically physical objects or concepts. A relation is usually described as a table, which is organized
+#into rows and columns. All the data referenced by an attribute are in the same domain and conform to the same constraints.
+
+'SQL'
+#Structured Query Language is the language we use to issue commands to the database
+#   -Create a table
+#   -Retrieve some data
+#   -Insert data
+#   -Delete data
