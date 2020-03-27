151148941d7df098629d4c6375498c60100e9231
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-26 18:56:39 -0700

    Create ObjectOrientedDefinitionsandTerminology.py

---
+++ Course 4 - Using Databases with Python/Chapter 14 - Objects/Lecture Material/ObjectOrientedDefinitionsandTerminology.py
@@ -0,0 +1,16 @@
+'Object Oriented'
+#A program is made up of many cooperating objects
+#Instead of being the "whole program" - each object is a little "island" within the pogram and cooperatively working with other objects
+#A program is made up of one or more objects working together - objects make use of each other's capabilities
+
+'Object'
+#*An object is a bit of self-contained Code and Data
+#A key aspecct of an Object approach is to break the problem into smaller understandable parts (divide and conquer)
+#Objects have boundaries that allow us to ignore un-needed detail.
+#We have been using objects all along: String Objects, Integer Objects, Dictionary Objects, List Objects...
+
+'Definitions'
+#Class - a template
+#Method or Message - A defined capability of a class
+#Field or attribute - A bit of data in a class
+#Object or Instance - A particular instance of a class
