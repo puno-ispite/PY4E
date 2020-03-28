d7305cc2ab224b7c0cc218fb9a308137fec18ce3
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-28 12:11:14 -0700

    Update tracks.py

--- Course 4 - Using Databases with Python/Chapter 16 - Data Models and Relational SQL/Lecture Materials/tracks/tracks.py
+++ Course 4 - Using Databases with Python/Chapter 16 - Data Models and Relational SQL/Lecture Materials/tracks/tracks.py
@@ -64,18 +64,18 @@ for entry in all:
     print(name, artist, album, count, rating, length)
 
     cur.execute('''INSERT OR IGNORE INTO Artist (name) 
-        VALUES ( ? )''', ( artist, ) )
+        VALUES ( ? )''', ( artist, ) ) #if its already there, dont insert it again. else do nothing
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
         VALUES ( ?, ? )''', ( album, artist_id ) )
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
-    album_id = cur.fetchone()[0]
+    album_id = cur.fetchone()[0] #grab album's foreign key for either existing or new row
 
     cur.execute('''INSERT OR REPLACE INTO Track
         (title, album_id, len, rating, count) 
         VALUES ( ?, ?, ?, ?, ? )''', 
-        ( name, album_id, length, rating, count ) )
+        ( name, album_id, length, rating, count ) ) #if the unique constraint would be violated, this turns into an update
 
     conn.commit()
