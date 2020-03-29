271e222a0d34acde2111c2370150fe567d6bf959
Ivan Hornung <ivanhor2003@gmail.com>
2020-03-28 19:33:41 -0700

    Update and rename ph.py to geodata.py

---
+++ Course 4 - Using Databases with Python/Chapter 18 - Geocoding/geodata.py
@@ -0,0 +1,6 @@
+'GeoData'
+#Makes a Google Map from user entered data
+#Uses the Google Geodata API
+#Catches data in a database to avoid rate limiting and allow restarting
+#Visualized in a browser using the Google Maps API
+#http://www.pythonlearning.com/code/geodata.zip
